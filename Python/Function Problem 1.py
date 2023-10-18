fd9f8574a27599bdd15de7a10d7bd0fa5f453af9
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-10-18 08:48:30 +0530

    Create Function Problem 1.py

---
+++ Python/Function Problem 1.py
@@ -0,0 +1,8 @@
+# https://course.acciojob.com/idle?question=ccb91ca8-4d6f-4dc1-aa9a-50781c91dd2d
+
+def functionNew():
+	print('I am another function')
+
+
+if __name__ == "__main__":
+	functionNew()
