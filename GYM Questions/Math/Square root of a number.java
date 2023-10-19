c585cc1f2761e3d8bbaa540c5dcd096fd3cd71d6
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-10-19 22:18:04 +0530

    Create Square root of a number.java

---
+++ GYM Questions/Math/Square root of a number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// https://course.acciojob.com/idle?question=911a3358-9285-40c4-b501-d5d24db1a103
+
+public class Main {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num  = sc.nextInt();
+        System.out.println((int)(Math.sqrt(num)));
+    }
+}
