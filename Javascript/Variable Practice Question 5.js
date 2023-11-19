e78e51570b77349bb594862bdebc027de9c894d6
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-19 16:37:03 +0530

    Create Variable Practice Question 5.js

---
+++ Javascript/Variable Practice Question 5.js
@@ -0,0 +1,5 @@
+// https://course.acciojob.com/idle?question=d2ab1301-e24c-47e3-abed-3421163ba5a8
+
+let v1 = 10.5, v2 = 12.5, v3 = 12.15;
+let average = Math.floor((v1+v2+v3)/3);
+console.log(average);
