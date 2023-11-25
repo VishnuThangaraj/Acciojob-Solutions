2ae76df2010a946cb6e07d79ee2073b795b4cc80
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-25 21:24:44 +0530

    Create Path Sum.java

---
+++ GYM Questions/Tree/Path Sum.java
@@ -0,0 +1,83 @@
+// https://course.acciojob.com/idle?question=bb1f6d0c-0338-40bc-a664-468c76e7cecd
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class Main {
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;   
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+            Scanner sc = new Scanner(System.in);
+			String s = sc.nextLine();
+		    int sum = sc.nextInt();
+			Node root = buildTree(s);
+			Solution tree = new Solution();
+			boolean result=tree.hasPathSum(root,sum);
+			System.out.print(result);
+	}
+}
+  
+
+
+class Solution{
+    public boolean hasPathSum(Node root, int sum) {
+        // Base Case
+        if(root == null) return false;
+	   if(root.left == null && root.right == null){
+           if(sum == root.data) return true;
+           return false;
+       }
+
+        return hasPathSum(root.right, sum - root.data) || hasPathSum(root.left, sum - root.data);
+    }
+}
