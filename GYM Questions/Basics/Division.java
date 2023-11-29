162373b871f16effa05bbbe4ee4bf0482bba1036
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-29 20:05:46 +0530

    Create Division.java

---
+++ GYM Questions/Basics/Division.java
@@ -0,0 +1,13 @@
+// https://course.acciojob.com/idle?question=98e7964a-2f46-4d48-ba45-f2d91e4c5eb6
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        int b = read.nextInt();
+        read.close();
+        System.out.println(a/b);
+    }
+}
