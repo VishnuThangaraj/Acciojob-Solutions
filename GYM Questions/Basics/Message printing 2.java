bcdc03d8b7c98982596b16f8a0512a252100ca01
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-29 19:51:22 +0530

    Create Message printing 2.java

---
+++ GYM Questions/Basics/Message printing 2.java
@@ -0,0 +1,13 @@
+// https://course.acciojob.com/idle?question=8e14fe76-d98c-4faf-9c13-77a17056d842
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    int age = sc.nextInt();
+    
+    System.out.println("My name is "+name+". I am "+age+" years old");
+  }
+}
