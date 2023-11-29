a6b6758f5113cc8a3a03e88be28efd0492843d50
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2023-11-29 20:03:38 +0530

    Create Multiplication.java

---
+++ GYM Questions/Math/Multiplication.java
@@ -0,0 +1,15 @@
+// https://course.acciojob.com/idle?question=9784d28d-335c-4349-803c-c3726957c759
+
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+       Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(n*m);
+        
+    }
+}
